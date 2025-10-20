08c852739ced23cbeb2edbe1fbef2f853ec0da38
Hugo Ulloa Fuentes <66287628+walapocha@users.noreply.github.com>
2025-10-20 20:03:02 -0300

    Add files via upload

---
+++ src/com/hugo2025/app/IniciarApp.java
@@ -0,0 +1,8 @@
+package com.hugo2025.app;
+public class IniciarApp {
+    public static void main(String[] args) {
+        // Crear una instancia de MenuPrincipal y llamar al método iniciar
+        MenuPrincipal inicio = new MenuPrincipal();
+        inicio.iniciar();
+    }
+}
---
+++ src/com/hugo2025/app/MenuPrincipal.java
@@ -0,0 +1,32 @@
+package com.hugo2025.app;
+
+import com.hugo2025.conversor.Divisas;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuPrincipal {
+    public void iniciar() {
+        // Carga el icono para mostrar en el cuadro de diálogo.
+        ImageIcon icon = new ImageIcon(getClass().getResource("/com/hugo2025/icons/icon-next.png"));
+
+        // Definir las opciones del menú
+        Object[] opciones = { "Conversor de Divisas", "Salir del Programa" };
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.GREEN);
+
+        Object elegirOpcion = JOptionPane.showInputDialog(null,
+                "Seleccione una opción:", "Menú Principal",
+                JOptionPane.QUESTION_MESSAGE, icon, opciones, opciones[0]);
+
+        if (elegirOpcion != null) {
+            String opcionElegida = (String) elegirOpcion;
+
+            if (opcionElegida.equals("Conversor de Divisas")) {
+                Divisas conversorDivisas = new Divisas();
+                conversorDivisas.convertirDivisa();
+            }
+        }
+    }
+}
